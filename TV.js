d6ea02c0a1641e6ed982138df73b7f06d50edb5d
vishi12 <77247243+vishi12@users.noreply.github.com>
2022-01-03 17:29:58 -0500

    Add files via upload

---
+++ TV.js
@@ -0,0 +1,49 @@
+img = "";
+objects = [];
+status = "";
+
+function preload() {
+    img = loadImage("TV.jpg");
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status - Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded")
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results){
+    if (error){
+        console.log(error);
+    }
+    else {
+        console.log(results);
+        objects = results;
+    }
+}
+
+function draw() {
+
+    image(img, 0, 0, 640, 420);
+
+    if (status != ""){
+       for (var i = 0 ; i < objects.length ; i++){
+           document.getElementById("status").innerHTML = "Status - Objects Detected";
+
+           fill("red");
+           percent = floor(objects[i].confidence * 100);
+           text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+           noFill();
+           stroke("red");
+           rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+       }
+    }
+}
